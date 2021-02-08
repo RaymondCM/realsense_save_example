f3723be4d41393aa785bc26c31072c387452fdd1
RaymondKirk <ray.tunstill@live.co.uk>
2021-02-08 16:01:09 +0000

    Save intrin/extrinsics

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import cv2
 
 import pathlib
 from datetime import datetime
+import json
+
+
+def cls_to_dict(obj):
+    return {k: getattr(obj, k, None) for k in dir(obj) if "__" not in k and not k.startswith("_")}
 
 
 def main():
+    visualise = False
     pipeline = rs.pipeline()
 
     config = rs.config()
@@ -43,32 +49,43 @@ def main():
             if not aligned_depth_frame or not color_frame or not o_depth_frame:
                 continue
             
-            # Render and save code
+            # Render
             o_depth_image = np.asanyarray(o_depth_frame.get_data())
             depth_image = np.asanyarray(aligned_depth_frame.get_data())
             color_image = np.asanyarray(color_frame.get_data())
 
-            # Visualisation code
-            depth_image_3d = np.dstack((depth_image, depth_image, depth_image))
-            depth_image_3d = (((depth_image_3d - depth_image_3d.min()) / (depth_image_3d.ptp())) * 255.0).astype(np.uint8)
+            with open(str(save_path / "{:07d}_info.json".format(current_image)), 'w') as f:
+                json.dump({
+                    "depth_intrinsics": cls_to_dict(o_depth_frame.profile.as_video_stream_profile().intrinsics),
+                    "aligned_depth_intrinsics": cls_to_dict(aligned_depth_frame.profile.as_video_stream_profile().intrinsics),
+                    "colour_intrinsics": cls_to_dict(color_frame.profile.as_video_stream_profile().intrinsics),
+                    "depth_to_color_extrinsics": cls_to_dict(o_depth_frame.profile.get_extrinsics_to(color_frame.profile)),
+                    "aligned_depth_to_color_extrinsics": cls_to_dict(aligned_depth_frame.profile.get_extrinsics_to(color_frame.profile)),
+                }, f, default=lambda obj: str(obj), indent=4)
 
+            time = str(datetime.time(datetime.now())).replace(".", "_").replace(":", "_")
             depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-            images = (color_image, depth_image_3d, depth_colormap)
-            cv2.namedWindow('RGB (Left) Aligned Depth to RGB (Middle) Depth Colour Visualisation (Right)', cv2.WINDOW_GUI_EXPANDED)
-            cv2.imshow('RGB (Left) Aligned Depth to RGB (Middle) Depth Colour Visualisation (Right)', np.hstack(images))
-
-            cv2.imwrite(str(save_path / "{:07d}_color.png".format(current_image)), color_image)
-            cv2.imwrite(str(save_path / "{:07d}_depth.png".format(current_image)), o_depth_image)
-            cv2.imwrite(str(save_path / "{:07d}_aligned_depth.png".format(current_image)), depth_image)
-            cv2.imwrite(str(save_path / "{:07d}_aligned_depth_vis.png".format(current_image)), depth_colormap)
+            cv2.imwrite(str(save_path / "{:07d}_{}_color.png".format(current_image, time)), color_image)
+            cv2.imwrite(str(save_path / "{:07d}_{}_depth.png".format(current_image, time)), o_depth_image)
+            cv2.imwrite(str(save_path / "{:07d}_{}_aligned_depth.png".format(current_image, time)), depth_image)
+            cv2.imwrite(str(save_path / "{:07d}_{}_aligned_depth_vis.png".format(current_image, time)), depth_colormap)
             print("Saved frames from iter {:07d} to '{}'".format(current_image, save_path.resolve()))
             current_image += 1
 
-            key = cv2.waitKey(1)
-            # Press esc or 'q' to close the image window
-            if key & 0xFF == ord('q') or key == 27:
-                cv2.destroyAllWindows()
-                break
+            # Visualisation code
+            if visualise:
+                depth_image_3d = np.dstack((depth_image, depth_image, depth_image))
+                depth_image_3d = (((depth_image_3d - depth_image_3d.min()) / (depth_image_3d.ptp())) * 255.0).astype(np.uint8)
+
+                images = (color_image, depth_image_3d, depth_colormap)
+                cv2.namedWindow('RGB (Left) Aligned Depth to RGB (Middle) Depth Colour Visualisation (Right)', cv2.WINDOW_GUI_EXPANDED)
+                cv2.imshow('RGB (Left) Aligned Depth to RGB (Middle) Depth Colour Visualisation (Right)', np.hstack(images))
+
+                key = cv2.waitKey(1)
+                # Press esc or 'q' to close the image window
+                if key & 0xFF == ord('q') or key == 27:
+                    cv2.destroyAllWindows()
+                    break
     finally:
         pipeline.stop()
 
