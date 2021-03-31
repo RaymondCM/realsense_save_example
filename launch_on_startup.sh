fc5b9ee7f44cc771c8fb60896bf39f77c988e2cf
RaymondKirk <ray.tunstill@live.co.uk>
2021-03-31 14:37:05 +0100

    Improve startup creator

--- launch_on_startup.sh
+++ launch_on_startup.sh
@@ -1,5 +1,8 @@
-sudo apt install gnome-startup-applications
+#!/usr/bin/env bash
+cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" || exit
+sudo apt install gnome-startup-applications --assume-yes
 mkdir -p ~/.config/autostart
+rm ~/.config/autostart/rs_capture.desktop || true
 cp rs_capture.desktop ~/.config/autostart/
 chmod +x .config/autostart/rs_capture.desktop
 echo "Please add 'user_name ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown'"
