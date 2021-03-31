2efe6deaa1dbdd2c407a090715548e21c8f61edd
RaymondKirk <ray.tunstill@live.co.uk>
2021-03-31 14:38:22 +0100

    Improve startup creator

--- launch_on_startup.sh
+++ launch_on_startup.sh
@@ -4,6 +4,6 @@ sudo apt install gnome-startup-applications --assume-yes
 mkdir -p ~/.config/autostart
 rm ~/.config/autostart/rs_capture.desktop || true
 cp rs_capture.desktop ~/.config/autostart/
-chmod +x .config/autostart/rs_capture.desktop
+chmod +x ~/.config/autostart/rs_capture.desktop
 echo "Please add 'user_name ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown'"
 echo "Script will reboot PC by default when crashed occur"
