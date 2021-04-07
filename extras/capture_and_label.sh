1f053f48ed3acfb9ceb2ed3e842c0ac27025ddbd
RaymondKirk <ray.tunstill@live.co.uk>
2021-04-07 20:13:16 +0100

    Update paths

--- extras/capture_and_label.sh
+++ extras/capture_and_label.sh
@@ -7,11 +7,11 @@ cd ..
 trap 'kill $(jobs -p)' EXIT
 
 source venv/bin/activate
-[ -f labelme ] || wget https://github.com/wkentaro/labelme/releases/download/v4.5.6/labelme-Linux -O labelme
-chmod +x labelme capture_and_label.desktop
+[ -f ./extras/labelme ] || wget https://github.com/wkentaro/labelme/releases/download/v4.5.6/labelme-Linux -O ./extras/labelme
+chmod +x ./extras/labelme extras/capture_and_label.desktop
 
 save_path=$(python -c "from rs_store.utils import get_new_save_path;print(get_new_save_path())")
 echo "Saving in $save_path"
 mkdir -p "$save_path"
-./labelme "$save_path" --autosave &
-python ../main.py --save --threads 1 --visualise --out "$save_path"
+./extras/labelme "$save_path" --autosave &
+python main.py --save --threads 1 --visualise --out "$save_path"
