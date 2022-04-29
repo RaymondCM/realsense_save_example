35fea9bee72c01dc6ceae29384ac7f932e6d5270
RaymondKirk <ray.tunstill@live.co.uk>
2022-04-29 12:14:29 +0100

    Add ability to write multiple configs

--- extras/supervisor/add_timelapse_capture.sh
+++ extras/supervisor/add_timelapse_capture.sh
@@ -2,18 +2,20 @@
 set -e
 cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" || exit
 
+config_name=${1:-"realsense-capture"}
+
 # Write config file
-sudo rm /etc/supervisor/conf.d/timelapse_capture.conf || true
+sudo rm /etc/supervisor/conf.d/${config_name}.conf || true
 [ -f tmp_tl.conf ] && sudo rm tmp_tl.conf
 cat << EOF > tmp_tl.conf
-[program:timelapse_capture]
-command=/usr/local/bin/timelapse_capture.sh
+[program:${config_name}]
+command=/usr/local/bin/${config_name}.sh
 autostart=true
 autorestart=true
-stderr_logfile=/var/log/timelapse_capture.err.log
-stdout_logfile=/var/log/timelapse_capture.out.log
+stderr_logfile=/var/log/${config_name}.err.log
+stdout_logfile=/var/log/${config_name}.out.log
 EOF
-sudo cp tmp_tl.conf /etc/supervisor/conf.d/timelapse_capture.conf
+sudo cp tmp_tl.conf /etc/supervisor/conf.d/${config_name}.conf
 sudo rm tmp_tl.conf
 
 # Get parameters from .env
@@ -22,9 +24,10 @@ INTERVAL=${INTERVAL:-600}
 THREADS=${THREADS:-1}
 WEBHOOK_URL=${WEBHOOK_URL:-""}
 HEALTHCHECK_URL=${HEALTHCHECK_URL:-""}
+CONFIG_PATH=${CONFIG_PATH:-""}
 SOURCE_DIRECTORY=$(pwd)
 
-sudo rm /usr/local/bin/timelapse_capture.sh || true
+sudo rm /usr/local/bin/${config_name}.sh || true
 [ -f tmp_tl.sh ] && sudo rm tmp_tl.sh
 cat << EOF > tmp_tl.sh
 #!/usr/bin/env bash
@@ -35,17 +38,18 @@ cd ../..
 source venv/bin/activate
 sleep 2
 
+
 echo "Starting with parameters:"
 echo -e "\tINTERVAL: ${INTERVAL}"
 echo -e "\tTHREADS: ${THREADS}"
 echo -e "\tWEBHOOK_URL: ${WEBHOOK_URL}"
 echo -e "\tHEALTHCHECK_URL: ${HEALTHCHECK_URL}"
-echo "-> python main.py --save --interval ${INTERVAL} --threads ${THREADS} --webhook ${WEBHOOK_URL} --health ${HEALTHCHECK_URL}"
+echo "-> python main.py --save --interval ${INTERVAL} --threads ${THREADS} --webhook ${WEBHOOK_URL} --health ${HEALTHCHECK_URL} --config ${CONFIG_PATH}"
 
-python main.py --save --interval "${INTERVAL}" --threads "${THREADS}" --webhook "${WEBHOOK_URL}" --health "${HEALTHCHECK_URL}"
+python main.py --save --interval "${INTERVAL}" --threads "${THREADS}" --webhook "${WEBHOOK_URL}" --health "${HEALTHCHECK_URL} --config ${CONFIG_PATH}"
 EOF
-sudo cp tmp_tl.sh /usr/local/bin/timelapse_capture.sh
-sudo chmod +x /usr/local/bin/timelapse_capture.sh
+sudo cp tmp_tl.sh /usr/local/bin/${config_name}.sh
+sudo chmod +x /usr/local/bin/${config_name}.sh
 sudo rm tmp_tl.sh
 
 sudo supervisorctl reread
